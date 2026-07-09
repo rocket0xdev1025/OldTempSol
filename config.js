9bc7d0e90b9861fa9ad4639ecb28a9c280e38d67
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-07-10 03:13:04 +0900

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const BOUNTY_CUP_CONFIG = {
-  contractAddress: "7FxskNY3K5t3uoedZ99D47iDnn9Z1p33d736FaWxpump",
+  contractAddress: "TBA",
   solMint: "So11111111111111111111111111111111111111112",
 };
 
